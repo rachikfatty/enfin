64027d4e766f734c6d60ded2756ba9877a5fe3db
Hafsa717 <76533609+Hafsa717@users.noreply.github.com>
2021-06-07 19:24:45 +0100

    Update test3.py

--- test3.py
+++ test3.py
@@ -3,7 +3,7 @@ from tkinter import *
 def Valider2():
 
     fenetre = Tk()
-    label1 = Label(fenetre, text="hada ghir test")
+    label1 = Label(fenetre, text="nadiaa a fatty")
     label1.grid(row=0, sticky=W)
 
     A = np.empty((50, 50)) #declaration de matrice vide
