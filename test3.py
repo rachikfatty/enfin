e27f9c008014a2572e8568f68f19666c2cc3ee6e
rachikfatty <85507111+rachikfatty@users.noreply.github.com>
2021-06-07 18:02:40 +0200

    Add files via upload

---
+++ test3.py
@@ -0,0 +1,9 @@
+import numpy as np
+from tkinter import *
+def Valider2():
+
+    fenetre = Tk()
+    label1 = Label(fenetre, text="hada ghir test")
+    label1.grid(row=0, sticky=W)
+
+    A = np.empty((50, 50)) #declaration de matrice vide
