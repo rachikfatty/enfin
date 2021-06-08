9fd80226f17921aa587e024706229705b55e1084
Hafsa717 <76533609+Hafsa717@users.noreply.github.com>
2021-06-08 19:44:19 +0100

    Update testA.py

--- testA.py
+++ testA.py
@@ -1,26 +1,29 @@
 import numpy as np
 
+m=2
+n=3
 
-m = 2
-n = 3
-b=0
-A = np.zeros((m+n,m*n)) # matrice de m+n lignes et m*n colonnes
+def remplireMatriceA(m,n):
+    b=0
+    c=0
+    A = np.zeros((m+n,m*n)) # matrice de m+n lignes et m*n colonnes
 
-for i in range(m):
-    k=1
-    for j in range(b,m*n):
-        if(k<=n):
+    for i in range(m):
+        k=1
+        for j in range(b,m*n):
+            if(k<=n):
+                A[i][j]=1
+                k += 1
+            else :
+                continue 
+        b += n
+    
+    for i in range(m,m+n):
+        for j in range(c,m*n,n):
             A[i][j]=1
-            k += 1
-        else :
-            continue 
-    b += n
-c=0
-for i in range(m,m+n):
-    for j in range(c,m*n,n):
-        A[i][j]=1
-    c += 1
+        c += 1
 
+    
 print(A)        
 
             
