1e247e281441211f72ae7ddbc7819e8395c26d0a
Hafsa717 <76533609+Hafsa717@users.noreply.github.com>
2021-06-12 18:47:23 +0100

    Update front_end.py

--- front_end.py
+++ front_end.py
@@ -1,27 +1,31 @@
 import back_end as back
 from tkinter import *
 
-root = Tk()
+def main_window():
+    root = Tk()
+    root.geometry("800x400")
 
-x = IntVar()
-y = IntVar()
+    x = IntVar()
+    y = IntVar()
 
-label1 = Label(root, text="Nombre de fournisseurs")
-label1.grid(row=0, sticky=W)
+    label1 = Label(root, text="Nombre de fournisseurs")
+    label1.grid(row=0, sticky=W, padx=10, pady=10)
 
-label2 = Label(root, text="Nombre de Clients")
-label2.grid(row=1, sticky=W)
+    label2 = Label(root, text="Nombre de Clients")
+    label2.grid(row=1, sticky=W, padx=10, pady=10)
 
-e = Entry(root, textvariable=x)
-f = Entry(root, textvariable=y)
+    e = Entry(root, textvariable=x)
+    f = Entry(root, textvariable=y)
 
-e.grid(row=0, column=1)
-f.grid(row=1, column=1)
+    e.grid(row=0, column=1, padx=10, pady=10)
+    f.grid(row=1, column=1, padx=10, pady=10)
 
-myButton1 = Button(root, text="Annuler", command=quit)
-myButton1.grid(row=3, column=1)
-myButton2 = Button(root, text="Valider", command=lambda: back.Valider(x.get(), y.get()))
-myButton2.grid(row=3, column=2)
+    myButton1 = Button(root, text="Annuler", command=quit)
+    myButton1.grid(row=3, column=1, padx=10, pady=10)
+
+    myButton2 = Button(root, text="Valider", command=lambda: back.Valider(x.get(), y.get()))
+    myButton2.grid(row=3, column=2, padx=10, pady=10)
 
 
-root.mainloop()
+
+    root.mainloop()
