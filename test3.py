5d8889b1188dd792e45866142948f8ab93dada06
Hafsa717 <76533609+Hafsa717@users.noreply.github.com>
2021-06-17 22:32:38 +0100

    Delete test3.py

--- test3.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from tkinter import *
-def Valider2():
-
-    fenetre = Tk()
-    label1 = Label(fenetre, text="nadiaa a fatty")
-    label1.grid(row=0, sticky=W)
-
-    A = np.empty((50, 50)) #declaration de matrice vide
